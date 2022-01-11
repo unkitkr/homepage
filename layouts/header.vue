633017fdf07f5913bd5377bf237b456eebd8782a
Ankit <ankit.official.dev@gmail.com>
2022-01-11 23:01:51 +0530

    Starting the project

---
+++ layouts/header.vue
@@ -0,0 +1,116 @@
+<template>
+  <div style="background-color: rgb(8, 10, 14)">
+    <b-navbar type="light" variant="transparent" class="container">
+      <b-navbar-brand to="/"
+        ><span class="align-baseline"><b-icon-slash-circle-fill class="logo"></b-icon-slash-circle-fill></span> <span class="logo-txt align-top"> Ankit Kumar</span></b-navbar-brand
+      >
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <div>
+          <b-dropdown id="dropdown-right" right class="m-md-2 btn-toggl" variant="transparent" no-caret>
+            <template #button-content><b-icon-caret-down-fill class="btn-menu"> </b-icon-caret-down-fill></template>
+            <b-dropdown-item
+              ><a :href="socials.twitter"><b-icon-twitter></b-icon-twitter></a
+            ></b-dropdown-item>
+            <b-dropdown-item
+              ><a :href="socials.linkedin"><b-icon-linkedin></b-icon-linkedin></a
+            ></b-dropdown-item>
+            <b-dropdown-item
+              ><a :href="socials.github"><b-icon-github></b-icon-github></a
+            ></b-dropdown-item>
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item><b-icon-info-circle-fill></b-icon-info-circle-fill></b-dropdown-item>
+            <b-dropdown-item><b-icon-text-paragraph></b-icon-text-paragraph></b-dropdown-item>
+          </b-dropdown>
+        </div>
+      </b-navbar-nav>
+    </b-navbar>
+    <Nuxt />
+  </div>
+</template>
+
+<style scoped>
+@media screen and (max-width: 800px) {
+  .nav-link {
+    margin-left: 0px !important;
+  }
+}
+.logo {
+  color: #00d395;
+  font-size: 28px;
+}
+.logo-txt {
+  font-weight: 700;
+  font-size: 20px;
+  color: rgb(255, 255, 255);
+  font-family: "Open sans";
+}
+.navbar {
+  height: 100px;
+}
+.navbar-toggler {
+  border: none !important;
+  color: rgb(0, 18, 44);
+}
+.nav-link {
+  margin-left: 8px;
+}
+.nav-link:hover {
+  color: rgb(0, 18, 44) !important;
+}
+.full-nav {
+  width: 100%;
+  height: 1px;
+  background-color: rgb(204, 204, 204);
+  box-shadow: 0px 1px 5px 0px rgb(204, 204, 204);
+}
+.btn-menu {
+  color: #00d395 !important;
+}
+a {
+  color: inherit;
+}
+a:hover {
+  color: #00d395;
+  text-decoration: none;
+}
+</style>
+
+<script lang="ts">
+import { BIconTextParagraph, BIconInfoCircleFill, BIconGithub, BIconLinkedin, BIconCaretDownFill, BIconSlashCircleFill, BIconTwitter, BIconInstagram, BIconLightningFill, BIconStopCircleFill, BIconSlashSquareFill, BIconFileFontFill, BIconQuestionSquareFill } from "bootstrap-vue";
+export default {
+  components: {
+    BIconSlashCircleFill,
+    BIconTwitter,
+    BIconInstagram,
+    BIconLightningFill,
+    BIconStopCircleFill,
+    BIconSlashSquareFill,
+    BIconFileFontFill,
+    BIconQuestionSquareFill,
+    BIconCaretDownFill,
+    BIconLinkedin,
+    BIconGithub,
+    BIconInfoCircleFill,
+    BIconTextParagraph,
+  },
+  data() {
+    return {
+      socials: {
+        twitter: "https://www.twitter.com/unkitkr",
+        linkedin: "https://www.linkedin.com/in/unkitkr",
+        github: "https://www.github.com/unkitkr",
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+$dropdown-min-width: 1rem;
+$dropdown-item-padding-y: 0.5rem;
+$dropdown-item-padding-x: 0.7rem;
+
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="margin">
+    <section class="hero">
+      <b-container>
+        <b-row>
+          <b-col>
+            <div class="intro-text">Hi.</div>
+            <div class="content-text">
+              <p>I am Ankit, a computer science graduate with a keen interest on product and design.</p>
+              <p>Currently I'm working at <a href="https://www.swapcard.com" style="color: #00d395">Swapcard</a> as a Backend Engineer in the Customer Integration Squad.</p>
+              <p>Previously I've worked at <a href="https://www.surveysparrow.com" style="color: #00d395">Surveysparrow</a> as a Product Engineer Intern in their Web performance Engineering team.</p>
+            </div>
+            <div class="content-sub-text">
+              <p>This is a work in progress, please consider visiting after next weekend, probably I'll have blogs and a bit more about me 😄</p>
+            </div>
+            <div class="content-text-links" style="margin-top: 2rem">
+              <p>
+                <a :href="socials.twitter"><b-icon-twitter></b-icon-twitter> twitter</a> &nbsp;&nbsp; <a :href="socials.linkedin"><b-icon-linkedin></b-icon-linkedin> linkedin</a> &nbsp;&nbsp; <a :href="socials.github"><b-icon-github></b-icon-github> github</a> &nbsp;&nbsp; <b-icon-text-paragraph></b-icon-text-paragraph> blogs
+              </p>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { BIconTextParagraph, BIconInfoCircleFill, BIconGithub, BIconLinkedin, BIconCaretDownFill, BIconSlashCircleFill, BIconTwitter, BIconInstagram, BIconLightningFill, BIconStopCircleFill, BIconSlashSquareFill, BIconFileFontFill, BIconQuestionSquareFill } from "bootstrap-vue";
+export default {
+  layout: "header",
+  components: {
+    BIconSlashCircleFill,
+    BIconTwitter,
+    BIconInstagram,
+    BIconLightningFill,
+    BIconStopCircleFill,
+    BIconSlashSquareFill,
+    BIconFileFontFill,
+    BIconQuestionSquareFill,
+    BIconCaretDownFill,
+    BIconLinkedin,
+    BIconGithub,
+    BIconInfoCircleFill,
+    BIconTextParagraph,
+  },
+  data() {
+    return {
+      socials: {
+        twitter: "https://www.twitter.com/unkitkr",
+        linkedin: "https://www.linkedin.com/in/unkitkr",
+        github: "https://www.github.com/unkitkr",
+      },
+    };
+  },
+};
+</script>
+<style scoped>
+@media screen and (max-width: 768px) {
+  .intro-text {
+    font-size: 3rem !important;
+  }
+  .content-text {
+    font-size: 1rem !important;
+  }
+  .content-sub-text {
+    font-size: 0.8rem !important;
+  }
+}
+@media screen and (max-width: 640) {
+  .intro-text {
+    font-size: 3rem !important;
+  }
+  .content-text {
+    font-size: 1rem !important;
+  }
+  .content-sub-text {
+    font-size: 0.8rem !important;
+  }
+}
+@media screen and (max-width: 340px) {
+  .intro-text {
+    font-size: 3rem !important;
+  }
+  .content-text {
+    font-size: 1rem !important;
+  }
+  .content-sub-text {
+    font-size: 0.8rem !important;
+  }
+}
+
+.hero {
+  min-height: calc(100vh - 100px);
+  background-color: rgb(8, 10, 14);
+  background-image: url("~assets/pattern-large-dots.svg");
+  background-repeat: repeat;
+  background-size: contain;
+}
+.intro-text {
+  font-size: 4rem;
+  margin-top: 8rem;
+  color: rgb(255, 255, 255);
+  font-weight: 700;
+  font-family: "Open sans";
+  margin-left: 5px;
+}
+
+.content-text {
+  margin-top: 1rem;
+  color: rgb(255, 255, 255);
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Open sans";
+  margin-left: 5px;
+}
+.content-text-links {
+  margin-top: 2rem;
+  color: rgb(255, 255, 255);
+  font-size: 1rem;
+  font-weight: 400;
+  font-family: "Open sans";
+  margin-left: 5px;
+}
+.content-sub-text {
+  margin-top: 2rem;
+  color: rgb(255, 255, 255);
+  font-size: 1rem;
+  font-weight: 400;
+  font-family: "Open sans";
+  margin-left: 5px;
+}
+a {
+  color: inherit;
+}
+a:hover {
+  color: #00d395;
+  text-decoration: none;
+}
+</style>
+
